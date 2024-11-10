5dbd39ea799b86cfad0f5c1ae7fce26764f505f5
firdoush <mdfirdoushali1776@gmail.com>
2024-11-10 16:35:48 +0530

    fix css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Appointment from "./pages/Appointment";
