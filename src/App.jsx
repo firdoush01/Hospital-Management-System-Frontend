fed6b195e1a1a7a4cf468adda29aab25a6abecf5
firdoush <mdfirdoushali1776@gmail.com>
2024-11-10 16:12:59 +0530

    navbar , hero and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,52 @@
-import React from "react"
-import {BrowserRouter as Router , Route, Routes} from "react-router-dom"
-import Home from "./pages/Home"
-import Appointment from "./pages/Appointment"
-import AboutUs from "./pages/AboutUs"
-import Register from "./pages/Register"
-import Login from "./pages/Login"
+import React, { useContext, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Appointment from "./pages/Appointment";
+import AboutUs from "./pages/AboutUs";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import { Context } from "./main";
+import axios from "axios";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-    return (
-        <>
-        <Router>
-            <Routes>
-                 <Route path='/' element={<Home/>}/>
-                 <Route path='/appointment' element={<Appointment/>}/>
-                 <Route path='/about' element={<AboutUs/>}/>
-                 <Route path='/register' element={<Register/>}/>
-                 <Route path='/login' element={<Login/>}/>
-                 
-                
-            </Routes>
-        </Router>
-        <ToastContainer position="top-center"></ToastContainer>
-        </>
-    )
-}
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/patient/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+  return (
+    <>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+      <ToastContainer position="top-center"></ToastContainer>
+    </>
+  );
+};
 
-export default App
+export default App;
